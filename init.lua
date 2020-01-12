5929bdee42f5080f450e7fd81c5c298c03e9647b
Kwaut <foden82@hotmail.com>
2020-01-12 08:19:10 +0100

    added slash cmds for raid management

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
-
 local frame = CreateFrame('Frame');
+local realm = GetRealmName();
 
 function initPulse ()
+	print('init pulse dkp module')
 	if ((Pulse_DKP == nil) or (Pulse_DKP.version < 1)) then
 		Pulse_DKP = {};
 	end
-
+	if (Pulse_DKP.realm == nil) then
+		Pulse_DKP.realm = {};
+	end
+	if (Pulse_DKP.realm[realm] == nil) then
+		Pulse_DKP.realm[realm] = {};
+	end
 	Pulse_DKP.version = 1;
 end
 
@@ -15,7 +21,7 @@ frame:SetScript('OnEvent', function (self, event, ...)
 		initPulse();
 		frame:RegisterEvent('LOOT_READY');
 
-		frame:SetScript('OnEvent', function (self, event, bagId)
+		frame:SetScript('OnEvent', function (self, event, bagId)			
 			if (event == 'LOOT_READY') then
 				dkpLootOpen();
 			end
